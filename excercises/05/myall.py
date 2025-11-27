b24e5036636d8d4749f356b25977c6accc112f64
Nicolas Reuter <nicolas.reuter98@gmail.com>
2025-11-27 19:09:31 +0100

    Add solutions for exercise sheet 05

---
+++ excercises/05/myall.py
@@ -0,0 +1,26 @@
+def all(lst: list[bool]) -> bool:
+    """Gibt True zurück, wenn alle Elemente in der Liste True sind, andernfalls False.
+
+    Args:
+        lst (list[bool]): Eine Liste von booleschen Werten.
+    """
+    for item in lst:
+        # if not item:
+        # if not item is True:
+        # if item is False:
+        if item is not True:
+            return False
+    return True
+
+
+assert all([]) is True
+assert all([True, True, True]) is True
+assert all([True, False, True, False]) is False
+
+"""
+Punktevergabe:
+- 2 Punkte für korrekte Implementierung und Typannotationen
+- da davon ausgegangen werden kann, dass nur boolsche Werte in der Liste sind,
+sind verschiedene Varianten zur Prüfung auf False ohne problematische Edge Cases möglich
+- -1 Punkt für fehlende Typannotationen
+"""
--- excercises/05/wordle.py
+++ excercises/05/wordle.py
@@ -1,15 +1,57 @@
 from wonderwords import RandomWord
 
 
-def generate_word() -> str:
-    """Generate a random word.
+def next_guess(word_length: int) -> str:
+    word = input("Nächster Tipp: ")
+    if len(word) != word_length:
+        print(f"Ihr Wort hat nicht die geforderte Länge {word_length}!")
+        return next_guess(word_length)
+    if len(set(word)) != len(word):
+        print("Ihr Wort enthält mehrfach vorkommende Buchstaben!")
+        return next_guess(word_length)
+    return word.upper()
 
-    Returns:
-        str: A random word.
-    """
+
+def analyze_guess(guess: str, solution: str) -> str:
+    # if len(guess) != len(solution):
+    #     raise ValueError("Länge von 'guess' und 'solution' müssen übereinstimmen.")
+    # guess = guess.upper()
+    # solution = solution.upper()
+
+    result = ""
+    for i in range(len(guess)):
+        if guess[i] == solution[i]:
+            result += guess[i]
+        elif guess[i] in solution:
+            result += guess[i].lower()
+        else:
+            result += "."
+    return result
+
+
+assert analyze_guess("RAUM", "KAUM") == ".AUM"
+assert analyze_guess("MAUER", "LAMPE") == "mA.e."
+assert analyze_guess("PAUSE", "OLIVE") == "....E"
+assert analyze_guess("WELT", "RAUM") == "...."
+
+
+def wordle() -> None:
     rw = RandomWord()
-    word = rw.word(word_min_length=4, word_max_length=7)
-    return word
-print(generate_word())
-print(generate_word())
-print(generate_word())
+    while True:
+        solution = rw.word(word_min_length=4, word_max_length=7).upper()
+        if len(set(solution)) == len(solution):
+            break
+
+    print(f"Das gesuchte Wort hat {len(solution)} Buchstaben!")
+
+    while True:
+        guess = next_guess(len(solution))
+        result = analyze_guess(guess, solution)
+        if result == solution:
+            print(f'Glückwunsch, Sie haben das Wort "{solution}" erraten! :)')
+            break
+        else:
+            print(result)
+
+
+wordle()
